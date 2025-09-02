686ef9c68c0821a8bd13c213562a6847015297eb
suhailrentback <suhail@rentback.app>
2025-09-02 22:57:25 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const host = req.headers.get("host") || "";
+  if (host.startsWith("status.")) {
+    const url = req.nextUrl.clone();
+    if (!url.pathname.startsWith("/status")) url.pathname = "/status";
+    return NextResponse.rewrite(url);
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/:path*"],
+};
 
