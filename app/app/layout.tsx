5bd2a5347d7d427d6c3eae63af706f24c112a3b4
suhailrentback <suhail@rentback.app>
2025-09-08 14:02:49 +0200

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,11 +1,16 @@
+// /app/app/layout.tsx
 import React from "react";
 import Script from "next/script";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Script id="rb-payments" strategy="beforeInteractive">
-        {`window.RB_PAYMENTS_ENDPOINT="https://script.google.com/macros/s/AKfycbwCqHgI_5wkWTTorP_803gULbkVDuhLLs_lQnKN9k5dl1NPJx7XKEHj8IOcIyIENZgm/exec";`}
+      <Script id="rb-config-nested" strategy="beforeInteractive">
+        {`
+          window.RB_WAITLIST_ENDPOINT = "https://script.google.com/macros/s/AKfycbwCqHgI_5wkWTTorP_803gULbkVDuhLLs_lQnKN9k5dl1NPJx7XKEHj8IOcIyIENZgm/exec";
+          window.RB_PAYMENTS_ENDPOINT  = "https://script.google.com/macros/s/AKfycbwCqHgI_5wkWTTorP_803gULbkVDuhLLs_lQnKN9k5dl1NPJx7XKEHj8IOcIyIENZgm/exec";
+          window.RB_WAITLIST_SECRET = "";
+        `}
       </Script>
       {children}
     </>
