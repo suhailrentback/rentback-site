52b6a181cc0bffbcae94dfd918960c459055f4d4
suhailrentback <suhail@rentback.app>
2025-09-08 14:23:09 +0200

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,15 +1,14 @@
-// /app/app/layout.tsx
 import React from "react";
 import Script from "next/script";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Script id="rb-config-nested" strategy="beforeInteractive">
+      <Script id="rb-payments" strategy="beforeInteractive">
         {`
-          window.RB_WAITLIST_ENDPOINT = "https://script.google.com/macros/s/AKfycbwCqHgI_5wkWTTorP_803gULbkVDuhLLs_lQnKN9k5dl1NPJx7XKEHj8IOcIyIENZgm/exec";
-          window.RB_PAYMENTS_ENDPOINT  = "https://script.google.com/macros/s/AKfycbwCqHgI_5wkWTTorP_803gULbkVDuhLLs_lQnKN9k5dl1NPJx7XKEHj8IOcIyIENZgm/exec";
-          window.RB_WAITLIST_SECRET = "";
+          window.RB_PAYMENTS_ENDPOINT = "https://script.google.com/macros/s/AKfycbwCqHgI_5wkWTTorP_803gULbkVDuhLLs_lQnKN9k5dl1NPJx7XKEHj8IOcIyIENZgm/exec";
+          // optional shared secret (leave blank if you don't want it)
+          window.RB_PAYMENTS_SECRET = "";
         `}
       </Script>
       {children}
