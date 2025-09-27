2ae4e9adb448e4dacf7f5bea1e09f20528a3827a
suhailrentback <suhail@rentback.app>
2025-09-27 17:03:04 +0200

    Update layout.tsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-export const dynamic = "force-static"; // safe to prerender
-// DO NOT add: export const runtime = "edge";
+export const dynamic = "force-static";
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
