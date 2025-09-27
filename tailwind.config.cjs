10c3a76d92e317f3b2da908cd83cb9fd62cb3b8c
suhailrentback <suhail@rentback.app>
2025-09-27 16:34:53 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
-// tailwind.config.js
-module.exports = {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: { extend: {} },
   plugins: [],
 };
+export default config;
